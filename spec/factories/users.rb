8ebe9674572d956806ee463072b445445ef2023a
UladzimirD <uladzimir.dranevich@gmail.com>
2014-03-21 13:11:18 +0300

    add tests for User

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,5 +2,7 @@
 
 FactoryGirl.define do
   factory :user do
+    sequence(:email) { |n| "user#{n}@user.com" }
+    password 'password'
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,20 @@
 require 'spec_helper'
 
 describe User do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:user) { build(:user) }
+
+  it 'has a valid factory' do
+    expect(user).to be_valid
+  end
+
+  context 'attributes' do
+    it { should respond_to(:email) }
+    it { should respond_to(:password) }
+    it { should respond_to(:password_confirmation) }
+  end
+
+  context 'validations' do
+    it { should validate_presence_of(:email) }
+    it { should validate_uniqueness_of(:email) }
+  end
 end
