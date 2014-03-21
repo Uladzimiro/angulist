c462f86094892bdaf39947d8b5e7cb38ff4f53a9
UladzimirD <uladzimir.dranevich@gmail.com>
2014-03-21 13:01:52 +0300

    install devise

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,4 +77,6 @@ Angulist::Application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
+
+  config.action_mailer.default_url_options = { host: 'vast-reef-5662.herokuapp.com' }
 end
